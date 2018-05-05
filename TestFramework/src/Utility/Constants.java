e1469a8481eefc0f52819972ce1597d21aed86d8
rajankumarpatel <rajankumarpatel@rajankumars-mbp.home>
2018-05-05 12:17:00 -0400

    Working code

--- TestFramework/src/Utility/Constants.java
+++ TestFramework/src/Utility/Constants.java
@@ -4,10 +4,10 @@ public class Constants {
 	
 	//filepath
 	
-	public static final String CONFIG_FILEPATH ="/Users/rajankumarpatel/Documents/workspace/TestFramework/Configurations/config.properties";
-	public static final String ENV_FILEPATH ="/Users/rajankumarpatel/Documents/workspace/TestFramework/Configurations/Enviornment.properties";
-	public static final String TESTJSON_FILEPATH ="/Users/rajankumarpatel/Documents/workspace/TestFramework/Configurations/test.json";
-	public static final String DEVJSON_FILEPATH ="/Users/rajankumarpatel/Documents/workspace/TestFramework/Configurations/Dev.json";
-	public static final String COMMONJSON_FILEPATH ="/Users/rajankumarpatel/Documents/workspace/TestFramework/Configurations/Common.json";
+	public static final String CONFIG_FILEPATH ="/Users/rajankumarpatel/Documents/workspace/Third-test/TestFramework/Configurations/config.properties";
+	public static final String ENV_FILEPATH ="/Users/rajankumarpatel/Documents/workspace/Third-test/TestFramework/Configurations/Enviornment.properties";
+	public static final String TESTJSON_FILEPATH ="/Users/rajankumarpatel/Documents/workspace/Third-test/TestFramework/Configurations/test.json";
+	public static final String DEVJSON_FILEPATH ="/Users/rajankumarpatel/Documents/workspace/Third-test/TestFramework/Configurations/Dev.json";
+	public static final String COMMONJSON_FILEPATH ="/Users/rajankumarpatel/Documents/workspace/Third-test/TestFramework/Configurations/Common.json";
 
 }
